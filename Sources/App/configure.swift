5c0fea1cf2e7c55c1448eb6a7fca4be089cda4ac
Robert Ignasiak <tori@robert-i.com>
2020-04-06 11:52:21 +0200

    Added leaf template system

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,6 +1,7 @@
 import Fluent
 import FluentSQLiteDriver
 import Vapor
+import Leaf
 
 // configures your application
 public func configure(_ app: Application) throws {
@@ -9,6 +10,9 @@ public func configure(_ app: Application) throws {
 
     app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
     app.migrations.add(CreateMockedResponse())
+
+    app.views.use(.leaf)
+    app.leaf.cache.isEnabled = false
 
     // register routes
     try routes(app)
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,7 +1,12 @@
 import Fluent
 import Vapor
+import Leaf
 
 func routes(_ app: Application) throws {
+    app.get { req in
+        req.view.render("index")
+    }
+
     app.get("*", use: getMockedResponseWithPath)
     app.get("*", "*", use: getMockedResponseWithPath)
     app.get("*", "*", "*", use: getMockedResponseWithPath)
@@ -12,10 +17,11 @@ func routes(_ app: Application) throws {
     app.post("responses", use: responseController.create)
 }
 
-func getMockedResponseWithPath(req: Request) throws -> EventLoopFuture<MockedResponse> {
+func getMockedResponseWithPath(req: Request) throws -> EventLoopFuture<String> {
     req.logger.info("\(req.url.string)")
     return MockedResponse.query(on: req.db)
         .filter(\.$path ~~ req.url.string)
         .first()
         .unwrap(or: Abort(.notFound))
+        .map { $0.content }
 }

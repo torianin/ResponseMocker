9c130997465cc2af43b3b4294336a6cc524b1ca2
Robert Ignasiak <tori@robert-i.com>
2020-05-10 01:42:22 +0200

    Add react router

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { BrowserRouter, Route } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
@@ -9,7 +10,11 @@ import reducers from './reducers';
 
 const App = () => {
     return (
-        <Dashboard />
+        <BrowserRouter>
+            <div>
+                <Route path="/" exact component={Dashboard} />
+            </div>
+        </BrowserRouter>
     )
 };
 

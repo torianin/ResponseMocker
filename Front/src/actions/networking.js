2903bc389a7a8c3d6fcb2a821d508b647a7e16d0
Robert Ignasiak <tori@robert-i.com>
2020-06-01 10:19:08 +0200

    Remove timeout to use default value

--- Front/src/actions/networking.js
+++ Front/src/actions/networking.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.BASE_URL,
-  timeout: 1000,
+  baseURL: process.env.BASE_URL
 });
 
 axiosInstance.interceptors.request.use((config) => {
